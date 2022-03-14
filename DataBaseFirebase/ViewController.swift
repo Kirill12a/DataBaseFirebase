5b85e39e4c9b08d49d368196ef0ee6159b823603
Kirill <kirilldrozd12@gmail.com>
2022-03-14 23:43:34 +0300

    sone change

--- DataBaseFirebase/ViewController.swift
+++ DataBaseFirebase/ViewController.swift
@@ -10,28 +10,40 @@ import FirebaseDatabase
 
 class ViewController: UIViewController {
   private let database = Database.database().reference()
+  var vtext:[String: Any]?
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    database.child( "something").observeSingleEvent(of: .value) { snapshot in
+    database.child("something48").observeSingleEvent(of: .value) { [self] snapshot in
       guard let value  = snapshot.value as? [String:Any] else {return  }
-
+      self.vtext = value
+      print(vtext ?? String())
       print("value: \(value)")
     }
 
+    buttonSettings()
+
+  }
+
+  private func buttonSettings(){
     let button = UIButton(frame: CGRect(x: 20, y: 200, width: view.frame.size.width - 40, height: 50  ))
     button.setTitle("Push me", for: .normal)
     button.setTitleColor(.white, for: .normal)
     button.backgroundColor = .link
     view.addSubview(button )
     button.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
+  }
 
-    }
+  private func labelMessage(){
+    let label = UILabel()
+    label.text = vtext!["something48"]  as? String
+  }
+
 
   @objc func addNewEntry(){
     let object: [String: Any] = [
-      "name":"Kikos" as NSObject ,
-      "YouTube":"Yes" 
+      "name":"Kirill" as NSObject ,
+      "YouTube":"Yes"
     ]
     database.child("something\(Int.random(in: 0...100))").setValue(object)
   }
